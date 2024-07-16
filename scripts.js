1c5f53004c572e1c5af5cdc141c4ef3b886b7bb9
Matheus Melo <teteuqe@hotmail.com>
2024-07-15 23:24:24 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let registro = document.querySelector(".registro")
+
+function aparecerregistro(){
+    registro.style.left = "50%"
+}
+function desaparecerregistro(){
+    registro.style.left = "-50%"
+}
